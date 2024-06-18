e64c5ae73b574cc7f210a3dff6213fd268a86e89
rgrs412 <yuwuheng0@gmail.com>
2024-06-18 17:08:51 -0400

    define pre-rendered dynamic paths for portfolio

--- src/app/(pages)/portfolio/[slug]/page.tsx
+++ src/app/(pages)/portfolio/[slug]/page.tsx
@@ -1,9 +1,17 @@
-import { notFound } from 'next/navigation';
+import { join } from "path";
+import type { Portfolio } from "@/types";
+import { getMdxContentByFileName } from "@/helpers/mdx";
+import { MdxContent } from "@/types";
+import { getMdxContent } from "@/helpers/mdx";
 
-const PortfolioPage = ({ params }: { params: { slug: string } }) => {
-    if (params.slug !== '123') {
-        notFound();
-    }
+export const dynamicParams = false;
+export async function generateStaticParams() {
+  const portfolioPath = join(process.cwd(), "src/app/data/portfolio");
+  const portfolios = await getMdxContent<Portfolio>(portfolioPath);
+  const paths = portfolios.map((p) => ({ slug: p.metadata.slug }));
+  
+  return paths;
+}
 
     return <div>My Post: {params.slug}</div>
   }
