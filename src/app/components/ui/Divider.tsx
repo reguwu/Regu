712ba4b9f148c77935c878efbf8e892e859f2060
rgrs412 <yuwuheng0@gmail.com>
2024-07-09 16:16:40 -0400

    fix divider alignment

--- src/app/components/ui/Divider.tsx
+++ src/app/components/ui/Divider.tsx
@@ -6,14 +6,16 @@ interface Props {
   orientation?: "h" | "v"
 }
 
+//For now, vertical divider with text is not supported
 const Divider = ({ text, margin=[1, 1, 1, 1], orientation="h"} : Props) => {
   const marginValue = `${margin[0]}rem ${margin[1]}rem ${margin[2]}rem ${margin[3]}rem`;
+  const horizontalTextMarginValue = `${margin[0]-0.7}rem ${margin[1]}rem ${margin[2]}rem ${margin[3]}rem`;
   return (
     <> 
       { text ?
         <div className={styles[`${orientation}divider-container`]}>
           <div className={styles[`${orientation}divider`]} style={{margin: marginValue}}></div>
-          <div className={styles["divider-text"]}>{text}</div>
+          <div className={styles["divider-text"]} style={{margin: horizontalTextMarginValue}}>{text}</div>
           <div className={styles[`${orientation}divider`]} style={{margin: marginValue}}></div>
         </div>
         :
