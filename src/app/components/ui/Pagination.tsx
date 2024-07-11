b59d5d7bacddbccd5ba3aae471ac03725831e768
rgrs412 <yuwuheng0@gmail.com>
2024-07-10 23:05:01 -0400

    refactor pagination

--- src/app/components/ui/Pagination.tsx
+++ src/app/components/ui/Pagination.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import styles from "@/styles/Pagination.module.css"
+import { useSearchParams, usePathname } from 'next/navigation';
 
 interface Props {
   totalPages: number
@@ -10,12 +11,30 @@ interface Props {
 export const Pagination: React.FC<Props> = ({totalPages, currentPage, query}) => {
   const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
   if(pageNumbers.length === 0) pageNumbers.push(1);
+  const searchParams = useSearchParams();
+  const params = new URLSearchParams(searchParams);
+  const pathname = usePathname();
 
+  const getPrevPageHref = () => {
+    params.set('page', (currentPage - 1).toString());
+    return `${pathname}?${params.toString()}`
+  }
+
+  const getSpecificPageHref = (page: number) => {
+    params.set('page', page.toString());
+    return `${pathname}?${params.toString()}`
+  }
+
+  const getNextPageHref = () => {
+    params.set('page', (currentPage + 1).toString());
+    return `${pathname}?${params.toString()}`
+  }
+  
   return (
     <div className={styles["pagination-container"]}>
       <div  className={styles["pagination"]}>
         <Link 
-          href={`/?${query === "" ? "" : `query=${query}&`}page=${currentPage - 1}#portfolio`} 
+          href={`${getPrevPageHref()}#portfolio`} 
           className={`${styles["pagination-item"]} ${currentPage === 1 ? styles["inactive"] : ""}`}
           replace
         > 
@@ -25,7 +44,7 @@ export const Pagination: React.FC<Props> = ({totalPages, currentPage, query}) =>
         {pageNumbers.map((page) => (
           <Link
             key={page}
-            href={`/?${query === "" ? "" : `query=${query}&`}page=${page}#portfolio`}
+            href={`${getSpecificPageHref(page)}#portfolio`}
             className={`${styles["pagination-item"]} ${currentPage === page ? styles["active"] : ""}`}
             replace
           >
@@ -34,7 +53,7 @@ export const Pagination: React.FC<Props> = ({totalPages, currentPage, query}) =>
         ))}
 
         <Link 
-          href={`/?${query === "" ? "" : `query=${query}&`}page=${currentPage + 1}#portfolio`} 
+          href={`${getNextPageHref()}#portfolio`} 
           className={`${styles["pagination-item"]} ${currentPage >= totalPages ? styles["inactive"] : ""}`}
           replace
         > 
