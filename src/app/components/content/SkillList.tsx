fbf366a7b9368b1d3c609609711f129e959e1092
rgrs412 <yuwuheng0@gmail.com>
2024-07-21 13:59:13 -0400

    trim extra icons on portfolio card

--- src/app/components/content/SkillList.tsx
+++ src/app/components/content/SkillList.tsx
@@ -3,27 +3,32 @@ import styles from "@/styles/SkillList.module.css";
 import Icon from "@/components/content/Icon";
 
 interface Props {
-  iconNames: Array<string>;
+  iconNames?: Array<string>;
   verbose?: boolean;
 }
 
 const SkillList: React.FC<Props> = ({ iconNames, verbose }) => {
+  const rowSize = 8;
+  const slicedIconNames: Array<string> | undefined = iconNames?.slice(0, rowSize);
+  const extraIconCount = (iconNames?.length ?? 0) - rowSize;
+
   return (
     <>
       {verbose ?
         <div className={styles["verbose-icons"]}>
-          {iconNames.map((name) => 
+          {iconNames?.map((name) => 
             <div key={name}>
-              <Icon name={name} size={30} />
-              <span>{name.toUpperCase()}</span>
+                <Icon name={name} size={25} />
+                <span>{name.toUpperCase()}</span>
             </div>
           )}
         </div>
       :    
         <div className={styles["icons"]}>
-          {iconNames.map((name) => 
-            <Icon key={name} name={name} size={20} />
+          {slicedIconNames?.map((name, index) => 
+            <Icon key={name} name={name} size={25} />
           )}
+          {extraIconCount > 0 ? <span>+{extraIconCount} ...</span> : null}
         </div>
       }
     </>
