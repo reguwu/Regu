fdd2c7eaa210f0833679e6d361d22ddd4ba07e26
rgrs412 <yuwuheng0@gmail.com>
2024-07-23 18:26:14 -0400

    sort portfolio by date

--- src/app/utils/portfolio/filterPortfolio.ts
+++ src/app/utils/portfolio/filterPortfolio.ts
@@ -3,12 +3,18 @@ import { Portfolio } from "@/types";
 /**
  * Filters an array of portfolios based on a query string.
  *
- * @param {Portfolio[]} portfolios - The array of portfolios to filter.
+ * @param {Portfolio[]} portfolio - The array of portfolio objects to filter.
  * @param {string} query - The query string to match against the portfolio's name, description, and tech stack.
  * @return {Portfolio[]} The filtered array of portfolios.
  */
-export const filterPortfolio = (portfolios: Portfolio[], query: string) => {
-  const filteredPortfolios = portfolios.filter((p) => {
+export const filterPortfolio = (portfolio: Portfolio[], query: string) => {
+
+  //sort by date in descending order
+  const sortedPortfolio = portfolio.sort((a, b) => {
+    return new Date(b.date).getTime() - new Date(a.date).getTime();
+  });
+
+  const filteredPortfolios = sortedPortfolio.filter((p) => {
     return (
       p.name?.toLowerCase().includes(query.toLowerCase()) ||
       p.description?.toLowerCase().includes(query.toLowerCase()) ||
