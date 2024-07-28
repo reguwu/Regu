1918d7926513fe3beb4b6b4b535a5172700d26de
rgrs412 <yuwuheng0@gmail.com>
2024-07-28 12:00:15 -0400

    fix mobile css

--- src/app/components/content/SocialMediaList.tsx
+++ src/app/components/content/SocialMediaList.tsx
@@ -2,17 +2,16 @@ import React from 'react'
 import { SOCIAL_MEDIAS } from '@/utils/constant'
 import Icon from '@/components/content/Icon'
 import styles from '@/styles/SocialMediaList.module.css'
-import Link from 'next/link';
 
 const SocialMediaList = () => {
   return (
     <div className={styles['social-icons']}>
       {SOCIAL_MEDIAS.map(({name, url}, index) => 
       name !== "" ?
-      <Link key={name} className={styles['clickable']} href={url} target="_blank" rel="noopener noreferrer">
+      <a key={name} className={styles['clickable']} href={url} target="_blank" rel="noopener noreferrer">
         <Icon name={name.toLowerCase()} size={60} />
         <div>{name}</div>
-      </Link>
+      </a>
       :
       <div key={index}>
         <Icon name={name} size={60} />
