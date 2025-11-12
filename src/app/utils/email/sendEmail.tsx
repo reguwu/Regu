63368fdc24b98a785228412bd76e6827ec24db71
rgrs412 <yuwuheng0@gmail.com>
2025-11-12 12:11:07 -0500

    disable send email

--- src/app/utils/email/sendEmail.tsx
+++ src/app/utils/email/sendEmail.tsx
@@ -5,25 +5,28 @@ import Email from "@/components/content/Email";
 import { EMAIL } from "@/utils/constant";
 
 export const sendEmail = async (prevState: any, formData: FormData) => {
-  const resend = new Resend(process.env.RESEND_API_KEY);
-  let toast: boolean = prevState.toast;
+  // const resend = new Resend(process.env.RESEND_API_KEY);
+  // let toast: boolean = prevState.toast;
 
-  await resend.emails.send({
-    from: EMAIL,
-    to: EMAIL,
-    reply_to: formData.get("email")?.toString(),
-    subject: `Email Via Resend: ${formData.get("email")?.toString()}`,
-    react: <Email msg={formData.get("message")?.toString()} />,
-  })
-  .then((data) => {
-    // data will be null if reply_to is invalid email
-    toast = data.data ? true : false;
+  // await resend.emails.send({
+  //   from: EMAIL,
+  //   to: EMAIL,
+  //   reply_to: formData.get("email")?.toString(),
+  //   subject: `Email Via Resend: ${formData.get("email")?.toString()}`,
+  //   react: <Email msg={formData.get("message")?.toString()} />,
+  // })
+  // .then((data) => {
+  //   // data will be null if reply_to is invalid email
+  //   toast = data.data ? true : false;
 
-    //for testing without actually sending email
-    // toast = formData.get("email")?.toString() === "aa" ? true : false;
-  })
+  //   //for testing without actually sending email
+  //   // toast = formData.get("email")?.toString() === "aa" ? true : false;
+  // })
 
+  // return {
+  //  toast: toast,
+  // }
   return {
-   toast: toast,
+   toast: true,
   }
 }
