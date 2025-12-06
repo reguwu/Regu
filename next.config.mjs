aeaaaef5ba4925ff81aac72b65bf10e6dc80ac4c
rgrs412 <yuwuheng0@gmail.com>
2025-12-06 09:34:44 -0500

    turbopack fix

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,9 @@ import withMDX from '@next/mdx';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  turbopack: {
+    // ...
+  },
   // transpilePackages: ['next-mdx-remote'],
   // experimental: {
   //   mdxRs: true
